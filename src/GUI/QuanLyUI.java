8e605bf246c47ccd4797111c814a73539c6ce078
huukhuong <huukhuong.it@gmail.com>
2021-04-07 22:04:38 +0700

    Sương sương giao diện bán hàng

--- src/GUI/QuanLyUI.java
+++ src/GUI/QuanLyUI.java
@@ -1,11 +1,14 @@
 package GUI;
 
 import DAO.MyConnect;
+import Main.Main;
 import static Main.Main.changLNF;
+import MyCustom.TransparentPanel;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 public class QuanLyUI extends JFrame {
 
@@ -33,14 +36,13 @@ public class QuanLyUI extends JFrame {
         this.setVisible(true);
     }
 
-    private JPanel pnTitle, pnCard, pnBanHang, pnNhapHang, pnSanPham, pnNhanVien, pnKhachHang;
-    ;
+    private JPanel pnTitle, pnMenuLeft, pnCard, pnBanHang, pnNhapHang, pnSanPham, pnNhanVien, pnKhachHang;
     private JLabel btnClose, btnMinimize, lblBanHang, lblNhapHang, lblSanPham, lblNhanVien, lblKhachHang;
     private final Color clLeftItem = new Color(63, 74, 89);
     private final Color clLeftItemHover = new Color(72, 88, 107);
     private final Color clLeftItemSelected = new Color(51, 202, 187);
     private ArrayList<JLabel> listMenuLeft;
-    private CardLayout cardGroup = new CardLayout();
+    private CardLayout cardMenuLeftGroup = new CardLayout();
 
     private void addControls() {
         int width = this.getWidth();
@@ -84,7 +86,7 @@ public class QuanLyUI extends JFrame {
                                 SIDE BAR MENU
         ============================================================
          */
-        JPanel pnMenuLeft = new JPanel();
+        pnMenuLeft = new JPanel();
         pnMenuLeft.setPreferredSize(new Dimension(250, height - pnTitle.getHeight()));
         pnMenuLeft.setBackground(clLeftItem);
         pnMenuLeft.setLayout(new BoxLayout(pnMenuLeft, BoxLayout.Y_AXIS));
@@ -123,7 +125,7 @@ public class QuanLyUI extends JFrame {
                                 CARD PANEL           
         ============================================================
          */
-        pnCard = new JPanel(cardGroup);
+        pnCard = new JPanel(cardMenuLeftGroup);
 
         pnBanHang = new JPanel();
         pnNhapHang = new JPanel();
@@ -131,17 +133,17 @@ public class QuanLyUI extends JFrame {
         pnNhanVien = new JPanel();
         pnKhachHang = new JPanel();
 
-        pnBanHang.setBackground(Color.BLACK);
-        pnNhapHang.setBackground(Color.BLUE);
-        pnSanPham.setBackground(Color.CYAN);
-        pnNhanVien.setBackground(Color.GRAY);
-        pnKhachHang.setBackground(Color.MAGENTA);
-
         pnCard.add(pnBanHang, "1");
         pnCard.add(pnNhapHang, "2");
         pnCard.add(pnSanPham, "3");
         pnCard.add(pnNhanVien, "4");
         pnCard.add(pnKhachHang, "5");
+
+        addControlsBanHang();
+        addControlsNhapHang();
+        addControlsSanPham();
+        addControlsNhanVien();
+        addControlsKhachHang();
 
         pnMain.add(pnCard);
         /*
@@ -240,7 +242,7 @@ public class QuanLyUI extends JFrame {
                     } else {
                         cardName = "5";
                     }
-                    cardGroup.show(pnCard, cardName);
+                    cardMenuLeftGroup.show(pnCard, cardName);
                 }
 
                 @Override
@@ -266,6 +268,56 @@ public class QuanLyUI extends JFrame {
                 }
             });
         }
+
+        lblTabbedBanHang.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                lblTabbedHoaDon.setIcon(tabbedDefault);
+                lblTabbedBanHang.setIcon(tabbedSelected);
+                cardBanHangGroup.show(pnTabbedBanHang, "1");
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+            }
+        });
+
+        lblTabbedHoaDon.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                lblTabbedHoaDon.setIcon(tabbedSelected);
+                lblTabbedBanHang.setIcon(tabbedDefault);
+                cardBanHangGroup.show(pnTabbedBanHang, "2");
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+            }
+        });
     }
 
     private void moverFrame(int x, int y) {
@@ -278,6 +330,230 @@ public class QuanLyUI extends JFrame {
 
     private void thoatChuongTrinh() {
         this.dispose();
+    }
+
+    private void addControlsKhachHang() {
+
+    }
+
+    private void addControlsSanPham() {
+
+    }
+
+    private void addControlsNhapHang() {
+
+    }
+
+    private JLabel lblTabbedBanHang, lblTabbedHoaDon;
+    private final ImageIcon tabbedSelected = new ImageIcon("image/ManagerUI/tabbed-btn--selected.png");
+    private final ImageIcon tabbedDefault = new ImageIcon("image/ManagerUI/tabbed-btn.png");
+    private final Color colorPanel = new Color(247, 247, 247);
+    private CardLayout cardBanHangGroup = new CardLayout();
+    private JPanel pnTabbedBanHang;
+    private JTable tblBanHang, tblGioHang;
+    private DefaultTableModel dtmBanHang, dtmGioHang;
+    private JTextField txtMaSPBanHang, txtTenSPBanHang, txtDonGiaBanHang, txtSoLuongBanHang;
+    private JComboBox<String> cmbLoaiSPBanHang;
+    private JLabel btnThemVaoGio, lblAnhSP;
+
+    private void addControlsBanHang() {
+        pnBanHang.setLayout(new FlowLayout());
+        pnBanHang.setBackground(colorPanel);
+
+        int w = (int) (this.getWidth() - pnMenuLeft.getPreferredSize().getWidth());
+        int h = (int) (this.getHeight() - pnTitle.getPreferredSize().getHeight());
+
+        /*
+        =========================================================================
+                                    PANEL TABBED
+        =========================================================================
+         */
+        Font font = new Font("", Font.PLAIN, 20);
+        JPanel pnTop = new TransparentPanel();
+        pnTop.setPreferredSize(new Dimension(w, 41));
+        pnTop.setLayout(null);
+        pnTop.setBorder(BorderFactory.createMatteBorder(1, 0, 1, 0, clLeftItem));
+
+        lblTabbedBanHang = new JLabel("Bán hàng");
+        lblTabbedBanHang.setHorizontalTextPosition(JLabel.CENTER);
+        lblTabbedBanHang.setVerticalTextPosition(JLabel.CENTER);
+        lblTabbedBanHang.setIcon(tabbedSelected);
+        lblTabbedBanHang.setBounds(2, 2, 140, 37);
+        lblTabbedBanHang.setFont(font);
+        lblTabbedBanHang.setForeground(Color.white);
+        lblTabbedBanHang.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+
+        lblTabbedHoaDon = new JLabel("Hoá đơn");
+        lblTabbedHoaDon.setHorizontalTextPosition(JLabel.CENTER);
+        lblTabbedHoaDon.setVerticalTextPosition(JLabel.CENTER);
+        lblTabbedHoaDon.setIcon(tabbedDefault);
+        lblTabbedHoaDon.setBounds(143, 2, 140, 37);
+        lblTabbedHoaDon.setFont(font);
+        lblTabbedHoaDon.setForeground(Color.white);
+        lblTabbedHoaDon.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+
+        pnTop.add(lblTabbedBanHang);
+        pnTop.add(lblTabbedHoaDon);
+
+        /*
+        =========================================================================
+                                    PANEL CT BÁN HÀNG
+        =========================================================================
+         */
+        JPanel pnCTBanHang = new TransparentPanel();
+        pnCTBanHang.setLayout(new BoxLayout(pnCTBanHang, BoxLayout.Y_AXIS));
+
+        JPanel pnTableBanHang = new TransparentPanel();
+        pnTableBanHang.setLayout(new BorderLayout());
+
+        //====================Bảng hàng hoá====================
+        JPanel pnTopBanHang = new TransparentPanel();
+        pnCTBanHang.add(pnTopBanHang);
+        pnTopBanHang.setLayout(new BoxLayout(pnTopBanHang, BoxLayout.X_AXIS));
+
+        Main.changLNF("Metal");
+        Main.changLNF("Windows");
+        dtmBanHang = new DefaultTableModel();
+        dtmBanHang.addColumn("Mã SP");
+        dtmBanHang.addColumn("Tên SP");
+        dtmBanHang.addColumn("Đơn giá");
+        dtmBanHang.addColumn("Số lượng");
+        dtmBanHang.addColumn("Đơn vị tính");
+        dtmBanHang.addColumn("Ảnh");
+        tblBanHang = new JTable(dtmBanHang);
+        JScrollPane scrTblBanHang = new JScrollPane(tblBanHang);
+
+        pnTableBanHang.add(scrTblBanHang, BorderLayout.CENTER);
+        pnTopBanHang.add(pnTableBanHang);
+
+        //====================Thông tin bán hàng====================
+        JPanel pnThongTinBanHang = new JPanel();
+        pnThongTinBanHang.setLayout(new BoxLayout(pnThongTinBanHang, BoxLayout.PAGE_AXIS));
+
+        JPanel pnLoaiSP = new TransparentPanel();
+        JLabel lblLoai = new JLabel("Loại SP");
+        lblLoai.setFont(font);
+        cmbLoaiSPBanHang = new JComboBox<>();
+        cmbLoaiSPBanHang.setFont(font);
+        loadDataComboboxLoaiBanSP();
+        pnLoaiSP.add(lblLoai);
+        pnLoaiSP.add(cmbLoaiSPBanHang);
+        pnThongTinBanHang.add(pnLoaiSP);
+
+        Main.changLNF("Nimbus");
+        JPanel pnMaSP = new TransparentPanel();
+        JLabel lblMa = new JLabel("Mã SP");
+        lblMa.setFont(font);
+        txtMaSPBanHang = new JTextField(15);
+        txtMaSPBanHang.setFont(font);
+        pnMaSP.add(lblMa);
+        pnMaSP.add(txtMaSPBanHang);
+        pnThongTinBanHang.add(pnMaSP);
+
+        JPanel pnTenSP = new TransparentPanel();
+        JLabel lblTen = new JLabel("Tên SP");
+        lblTen.setFont(font);
+        txtTenSPBanHang = new JTextField(15);
+        txtTenSPBanHang.setFont(font);
+        pnTenSP.add(lblTen);
+        pnTenSP.add(txtTenSPBanHang);
+        pnThongTinBanHang.add(pnTenSP);
+
+        JPanel pnDonGiaSP = new TransparentPanel();
+        JLabel lblDonGia = new JLabel("Đơn giá");
+        lblDonGia.setFont(font);
+        txtDonGiaBanHang = new JTextField(15);
+        txtDonGiaBanHang.setFont(font);
+        pnDonGiaSP.add(lblDonGia);
+        pnDonGiaSP.add(txtDonGiaBanHang);
+        pnThongTinBanHang.add(pnDonGiaSP);
+
+        JPanel pnSoLuongSP = new TransparentPanel();
+        JLabel lblSoLuong = new JLabel("Số lượng");
+        lblSoLuong.setFont(font);
+        txtSoLuongBanHang = new JTextField(15);
+        txtSoLuongBanHang.setFont(font);
+        pnSoLuongSP.add(lblSoLuong);
+        pnSoLuongSP.add(txtSoLuongBanHang);
+        pnThongTinBanHang.add(pnSoLuongSP);
+
+        JPanel pnButtonBan = new TransparentPanel();
+        btnThemVaoGio = new JLabel(new ImageIcon("image/ManagerUI/btn-BanHang.png"));
+        pnButtonBan.add(btnThemVaoGio);
+        pnThongTinBanHang.add(pnButtonBan);
+
+        cmbLoaiSPBanHang.setPreferredSize(txtMaSPBanHang.getPreferredSize());
+        Dimension sizeLabel = lblSoLuong.getPreferredSize();
+        lblLoai.setPreferredSize(sizeLabel);
+        lblMa.setPreferredSize(sizeLabel);
+        lblTen.setPreferredSize(sizeLabel);
+        lblDonGia.setPreferredSize(sizeLabel);
+
+        pnTopBanHang.add(pnThongTinBanHang);
+
+        //====================Thông tin giỏ hàng====================
+        JPanel pnBottomBanHang = new TransparentPanel();
+        pnBottomBanHang.setLayout(new BoxLayout(pnBottomBanHang, BoxLayout.X_AXIS));
+
+        JPanel pnTableGioHang = new TransparentPanel();
+        pnTableGioHang.setLayout(new BorderLayout());
+
+        Main.changLNF("Metal");
+        Main.changLNF("Windows");
+        dtmGioHang = new DefaultTableModel();
+        dtmGioHang.addColumn("Tên SP");
+        dtmGioHang.addColumn("Số lượng");
+        dtmGioHang.addColumn("Đơn giá");
+        dtmGioHang.addColumn("Thành tiền");
+
+        tblGioHang = new JTable(dtmGioHang);
+        JScrollPane scrTblGioHang = new JScrollPane(tblGioHang);
+        pnTableGioHang.add(scrTblGioHang, BorderLayout.CENTER);
+
+        JPanel pnAnhSP = new TransparentPanel();
+        pnAnhSP.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, Color.GRAY));
+        pnAnhSP.setPreferredSize(new Dimension(
+                (int) pnThongTinBanHang.getPreferredSize().getWidth(),
+                (int) ((int) h - pnTitle.getHeight() - pnThongTinBanHang.getPreferredSize().getHeight())));
+        lblAnhSP = new JLabel();
+        lblAnhSP.setPreferredSize(new Dimension(200, 200));
+        lblAnhSP.setBorder(BorderFactory.createLineBorder(clLeftItemSelected));
+        JLabel lblTextAnh = new JLabel("Ảnh minh hoạ", JLabel.CENTER);
+        lblTextAnh.setFont(font);
+        lblTextAnh.setPreferredSize(new Dimension((int) pnAnhSP.getPreferredSize().getWidth(), 30));
+        pnAnhSP.add(lblTextAnh);
+        pnAnhSP.add(lblAnhSP);
+
+        pnBottomBanHang.add(pnTableGioHang);
+        pnBottomBanHang.add(pnAnhSP);
+
+        pnCTBanHang.add(pnBottomBanHang);
+        /*
+        =========================================================================
+                                    PANEL CT HOÁ ĐƠN
+        =========================================================================
+         */
+        JPanel pnCTHoaDon = new JPanel();
+
+        pnTabbedBanHang = new JPanel(cardBanHangGroup);
+        pnTabbedBanHang.add(pnCTBanHang, "1");
+        pnTabbedBanHang.add(pnCTHoaDon, "2");
+        pnTabbedBanHang.setPreferredSize(new Dimension(w, (int) (h - pnTop.getPreferredSize().getHeight())));
+
+        pnBanHang.add(pnTop);
+        pnBanHang.add(pnTabbedBanHang);
+    }
+
+    private void addControlsNhanVien() {
+
+    }
+
+    private void loadDataComboboxLoaiBanSP() {
+        cmbLoaiSPBanHang.addItem("0 - Chọn loại");
+
+        //fake data trước, đoạn này đổ database
+        cmbLoaiSPBanHang.addItem("1 - Pizza");
+        cmbLoaiSPBanHang.addItem("2 - Nước uống");
     }
 
 }
