b30f8c58589b33aceee2ac31f2b00d6761607b78
sdphat <saudaiphat@gmail.com>
2021-04-18 19:04:20 +0700

    - Sửa thuộc tính hoTen thành ho, ten ở KhachHangDAO, NhanVienDAO, KhachHangDTO, NhanVienDTO

--- src/DAO/KhachHangDAO.java
+++ src/DAO/KhachHangDAO.java
@@ -25,9 +25,10 @@ public class KhachHangDAO {
             while(rs.next()) {
                 KhachHang kh = new KhachHang();
                 kh.setMaKH(rs.getInt(1));
-                kh.setHoTen(rs.getString(2));
-                kh.setGioiTinh(rs.getString(3));
-                kh.setTongChiTieu(rs.getInt(4));
+                kh.setHo(rs.getString(2));
+                kh.setTen(rs.getString(3));
+                kh.setGioiTinh(rs.getString(4));
+                kh.setTongChiTieu(rs.getInt(5));
                 dskh.add(kh);
             }            
         } catch (SQLException ex) {
@@ -46,9 +47,10 @@ public class KhachHangDAO {
             while(rs.next()) {
                 kh = new KhachHang();
                 kh.setMaKH(rs.getInt(1));
-                kh.setHoTen(rs.getString(2));
-                kh.setGioiTinh(rs.getString(3));
-                kh.setTongChiTieu(rs.getInt(4));
+                kh.setHo(rs.getString(2));
+                kh.setTen(rs.getString(3));
+                kh.setGioiTinh(rs.getString(4));
+                kh.setTongChiTieu(rs.getInt(5));
             }
         } catch (SQLException ex) {
             return null;
@@ -62,9 +64,10 @@ public class KhachHangDAO {
             String sql = "INSERT INTO khachhang VALUES(?,?,?,?)";
             PreparedStatement prep = MyConnect.conn.prepareStatement(sql);
             prep.setInt(1, kh.getMaKH());
-            prep.setString(2, kh.getHoTen());
-            prep.setString(3, kh.getGioiTinh());
-            prep.setInt(4, kh.getTongChiTieu());
+            prep.setString(2, kh.getHo());
+            prep.setString(3, kh.getTen());
+            prep.setString(4, kh.getGioiTinh());
+            prep.setInt(5, kh.getTongChiTieu());
             result = prep.executeUpdate() > 0;
         } catch(SQLException ex) {
             return false;
@@ -91,10 +94,11 @@ public class KhachHangDAO {
             String sql = "UPDATE khachhang SET MaKH=?, HoTen=?, GioiTinh=?, TongChiTieu=? WHERE MaKH=?";
             PreparedStatement prep = MyConnect.conn.prepareStatement(sql);
             prep.setInt(1, kh.getMaKH());
-            prep.setString(2, kh.getHoTen());
-            prep.setString(3, kh.getGioiTinh());
-            prep.setInt(4, kh.getTongChiTieu());
-            prep.setInt(5, maKH);
+            prep.setString(2, kh.getHo());
+            prep.setString(3, kh.getTen());
+            prep.setString(4, kh.getGioiTinh());
+            prep.setInt(5, kh.getTongChiTieu());
+            prep.setInt(6, maKH);
             result = prep.executeUpdate() > 0;
         } catch(SQLException ex) {
             return false;
