5d62d074cbc7054ddcedf27b6076717af6618327
huukhuong <huukhuong.it@gmail.com>
2021-04-21 19:44:30 +0700

    Thêm đọc dữ liệu Connect từ file

--- src/DAO/MyConnect.java
+++ src/DAO/MyConnect.java
@@ -3,6 +3,9 @@ package DAO;
 import MyCustom.MyDialog;
 import com.mysql.jdbc.Driver;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Properties;
@@ -34,9 +37,26 @@ public class MyConnect {
 
     private void docFileText() {
         // Xử lý đọc file để lấy ra 4 tham số
-        severName = "localhost";
-        dbName = "quanlypizza";
-        userName = "root";
+        severName = "";
+        dbName = "";
+        userName = "";
         password = "";
+
+        try {
+            FileInputStream fis = new FileInputStream("ConnectVariable.txt");
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+
+            severName = br.readLine();
+            dbName = br.readLine();
+            userName = br.readLine();
+            password = br.readLine();
+
+            if (password == null) {
+                password = "";
+            }
+
+        } catch (Exception e) {
+        }
     }
 }
